e68b9a4d0ca2cc7bc2c3d61ebbd01172fc3b971e
Yurii <116266510+xtxthby@users.noreply.github.com>
2023-06-19 00:43:43 +0300

    add files 1

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -109,9 +109,7 @@ export const ContactForm = () => {
           <FieldFormik
             type="text"
             name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
+            placeholder="Name" 
           />
           <ErrorMessage name="name" component="span" />
         </FormField>
@@ -122,9 +120,7 @@ export const ContactForm = () => {
           <FieldFormik
             type="tel"
             name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
+            placeholder="+38-067-123-45-67"
           />
           <ErrorMessage name="number" component="span" />
         </FormField>
